6b8087cd30e05d3f616f22bdadb0896b0a3e1070
tomk1002 <96536866+tomk1002@users.noreply.github.com>
2025-09-07 18:19:16 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -3,17 +3,15 @@ def hansu():
     count = 0
 
     if n < 100:
-        count = n
-        return count
+        return n
     else:
         count+=99
 
-    for i in range(100,n+1):
+    for i in range(111,n+1):
         num = str(i)
         if int(num[2])-int(num[1]) == int(num[1])-int(num[0]):
             count+=1
 
     return count
-
 
 print(hansu())
