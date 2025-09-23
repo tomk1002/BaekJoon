643f7c6aa2f097851cec61adce7776968d246eb5
tomk1002 <96536866+tomk1002@users.noreply.github.com>
2025-09-23 11:50:04 +0900

    [Gold III] Title: 아침 산책, Time: 832 ms, Memory: 104200 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,41 +1,41 @@
 import sys
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+inside = '0' + input()  # 각 노드가 실내(1)인지 실외(0)인지 저장한 문자열
+# 노드 번호를 index로 접근하기 위해 앞에 0 추가
+
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+total = 0 # 경로의 개수를 저장할 변수
+
+
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+    # 간선이 주어졌을 때 두 정점 모두 실내이면 양방향 경로의 수 2 증가
+    if inside[a] == inside[b] == "1":
+        total += 2
+
 
 def DFS(start):
-    stk = [start]
-    while stk:
-        x = stk.pop()
-        visited[x] = True
-        for nxt in arr[x]:
-            if not visited[nxt]:
-                visited[nxt] = True
-                if inout[nxt-1] == 1:
-                    routes.append(1)
-                elif inout[nxt-1] == 0:
-                    stk.append(nxt)
-            
-
-N = int(sys.stdin.readline())
-
-# get indoor/outdoor states - string ==> int list conversion
-inout = list(map(int, sys.stdin.readline().rstrip()))
-
-# get vertexes into adjacent list
-arr = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    u,v = map(int,sys.stdin.readline().split())
-    arr[u].append(v)
-    arr[v].append(u)
+    visited[start] = True # 방문 처리
+    inside_count = 0 # 현재 노드와 인접한 실내 노드 개수를 저장할 변수
+    for nxt in graph[start]:
+        # 인접한 노드가 실내라면
+        if inside[nxt] == '1': 
+            inside_count += 1 # 실내 노드 개수 증가
+        # 인접한 노드가 실외라면
+        elif not visited[nxt] and inside[i] == "0": # 그 노드에서부터 탐색하여 실내 노드 개수 증가
+            inside_count += DFS(nxt)
+    return inside_count # 실내 노드 개수 반환
 
 
-# 실내만이 시작과 끝. 실내 = 1, 실외 = 0
-# 중간에 실내 있으면 X
-# 실내 노드 기준으로 ㄱㄱ
+for i in range(1, N+1):
+    if inside[i] == '0' and not visited[i]: # 시작이 실외일 때만 탐색
+        result = DFS(i) # DFS를 통해 인접한 실내 노드 개수를 계산
+        total += (result) * (result - 1) # 경로의 개수 추가
 
-routes = []
-for j in range(1,N+1):
-    visited = [False]*(N+1)
-    if inout[j-1] == 1:
-        DFS(j)
-
-print(len(routes))
-
+print(total)
