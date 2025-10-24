a4a83cd67dfc4aaeda71a498df52c46ca2dac18e
tomk1002 <96536866+tomk1002@users.noreply.github.com>
2025-10-24 09:17:54 +0900

    [Silver IV] Title: 수 찾기, Time: 232 ms, Memory: 51188 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,13 @@
 import sys
+import bisect
 
-x = int(input())
-a = list(map(int, sys.stdin.readline().split()))
-a.sort()
-y = int(input())
-m = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+numbers = list(map(int,sys.stdin.readline().split()))
+numbers.sort()
 
-def bin_search(a,n):
-    pl = 0
-    pr = len(a)-1
+M = int(input())
+given = list(map(int,sys.stdin.readline().split()))
 
-    while pl <= pr:
-        pc = (pl+pr)//2
-        if n == a[pc]:
-            return True
-        if n > a[pc]:
-            pl = pc+1
-        else:
-            pr = pc-1
-    return False
-    
-for num in m:
-    if bin_search(a,num):
-        print(1)
-    else:
-        print(0)
+for number in given:
+    idx = bisect.bisect_left(numbers,number)
+    print(int(idx < N and numbers[idx] == number))
