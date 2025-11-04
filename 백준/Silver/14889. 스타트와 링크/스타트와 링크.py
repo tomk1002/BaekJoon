aa32f03ac3a40ab4a9959f2b42195a9ac01a6358
tomk1002 <96536866+tomk1002@users.noreply.github.com>
2025-11-04 10:52:17 +0900

    [Silver I] Title: 스타트와 링크, Time: 1004 ms, Memory: 57548 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,27 +5,31 @@ input = sys.stdin.readline
 
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N)]
-
-min_diff = float('inf')
 players = list(range(N))
+min_diff = float('inf')
 
-for team_a in combinations(players, N // 2):
-    team_b = list(set(players) - set(team_a))
+def team_score(team):
+    score = 0
+    for i in range(len(team)):
+        for j in range(i + 1, len(team)):
+            x, y = team[i], team[j]
+            score += S[x][y] + S[y][x]
+    return score
 
-    # 능력치 계산 함수
-    def team_score(team):
-        score = 0
-        for i in range(len(team)):
-            for j in range(i + 1, len(team)):
-                x, y = team[i], team[j]
-                score += S[x][y] + S[y][x]
-        return score
+# 조합 절반만 보기
+combs = list(combinations(players, N // 2))
+half = len(combs) // 2
+
+for i in range(half):
+    team_a = combs[i]
+    team_b = combs[-i - 1]  # 반대쪽 조합은 team_b로 간주
 
     score_a = team_score(team_a)
     score_b = team_score(team_b)
-    min_diff = min(min_diff, abs(score_a - score_b))
+    diff = abs(score_a - score_b)
+    min_diff = min(min_diff, diff)
 
     if min_diff == 0:
-        break  # 더 이상 줄일 수 없음
+        break
 
 print(min_diff)
